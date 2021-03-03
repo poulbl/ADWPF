49b56fa30f0f4c7614033d00972052e26536c680
Poul <poulbachlauritsen@gmail.com>
2021-03-03 13:25:05 +0100

    init wpf
    good times

---
+++ APWPF/APWPF/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADWPF
+{
+    class Session
+    {
+        bool loggedIn = false;
+
+        public void Login()
+        {
+
+        }
+    }
+}
